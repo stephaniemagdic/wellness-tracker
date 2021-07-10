faae62fa2c7a263e83685d8d53e391eb2a4a3b51
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-10 13:02:44 -0600

    Add User class skeleton and file paths to test

---
+++ src/User.js
@@ -0,0 +1,20 @@
+class User {
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
+  }
+
+  returnFirstName(id) {
+   //const firstName = this.name //.split(' ');
+   //return firstName;
+  }
+
+}
+
+export default User;
+
