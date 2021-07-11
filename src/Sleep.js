5ce37c078b7b1f53d193ef1dc12bdd74d67cfad8
Sarah <sarahrudy@gmail.com>
2021-07-11 12:49:14 -0600

    Combine SleepRespository class with Sleep class

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,77 +1,66 @@
 import User from "./User"
 
-class SleepData {
-  constructor(arrayofSleepUserObjects) {
-    constructor(sleepRepoInstance.filterUserById(id)) {
-  //an array of all the objects that hold one users data.
-    // [{"userID":2,"date":"2019/06/15","hoursSlept":7,"sleepQuality":4.7},
-    // {"userID":2,"date":"2019/06/16","hoursSlept":7.5,"sleepQuality":3.8},
-    // {"userID":2,"date":"2019/06/17","hoursSlept":5.7,"sleepQuality":3},
-    //
-    //
-    //
-
-      this.arrayofSleepUserObjects = arrayofSleepUserObjects;
- }
+class Sleep {
+  constructor(userID, sleepData) {
+    this.allUsersSleep = sleepData;
+    this.oneUsersSleep = this.allUsersSleep.filter(sleepElement => sleepElement.userID === userID)
+  } 
 
   calculateDailyAverage() {
-    //For a user (identified by their userID), the average number of hours slept per day
+    //this.oneUsersSleep - the average number of hours slept per day
   }
 
-  calculateAverageSleepQuality() {
-
+  calculateAverageSleepQuality(date) {
+    // get all instances where id === this.oneUsersSleep (.filter())
   }
 
   filterHoursByDate(date) {
-//For a user, how many hours they slept for a specific day (identified by a date)
+    //For a user, how many hours they slept for a specific day (identified by a date)
 
-    //const data = this.arrayofSleepUserObjects.filter((object) => object.date = date);
-
+    //const data = this.allUsersSleep.filter((object) => object.date = date);
     //return data.hoursSlept;
   }
 
   filterQualityByDate(date) {
- 
-  //For a user, their sleep quality for a specific day (identified by a date)
-    //const data = this.arrayofSleepUserObjects.filter((object) => object.date = date);
+    //For a user, their sleep quality for a specific day (identified by a date)
+    //const data = this.allUsersSleep.filter((object) => object.date = date);
 
     //return data.hoursSlept.sleepQuality;
-
   }
 
   filterHoursByWeek(date) {
     // For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 
-      //const data = this.arrayofSleepUserObjects.filter((object) => object.date > date && < object.date = date - 7);
+    //const data = this.allUsersSleep.filter((object) => object.date > date && < object.date = date - 7);
 
     //return data.map((date) => return date.hoursSlept)
-    
-
-
-
   }
 
-  filterQualityByWeek(id, date) {
+  filterQualityByWeek(date) {
      //filterUserById(id) 
      // For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 
-    //const data = this.arrayofSleepUserObjects.filter((object) => object.date > date && < object.date = date - 7);
+    //const data = this.allUsersSleep.filter((object) => object.date > date && < object.date = date - 7);
 
-    //return data.map((date) => return date.sleepQuality)
-    
+    //return data.map((date) => return date.sleepQuality)   
+  }
+  calculateAllUsersAverageSleepQuality() {
+    //For all users, the average sleep quality
+  }
+  
+  filterUsersSleepQualityAboveThree(week) {
+    // Find all users who average a sleep quality greater than 3 for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  }
+
+  findBestSleepersByDay(date) {
+    // For a given day (identified by the date), find the users who slept the most number of hours (one or more if they tied)
+  }
+  
+// HELPER METHOD
+   filterUserById(id) {
+   // return this.sleepData.filter((user) => user.id === id
+   // this will return an array 
   }
 
 
-
-
-
 export default Sleep;
-
-
-
-//WE MAY WANT TO CREATE A SLEEP REPOSITORY CLASS FOR THIS DATA
-  //user's week sleep quality
-  // sleep.filterQualityByWeek;
-
-  //all users averageSleepQuality
-  // sleep.calculateAverageSleepQuality;
