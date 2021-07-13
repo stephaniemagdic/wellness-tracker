a9011cd3f75c3f015a8dd54b9d142d7619415706
Sarah <sarahrudy@gmail.com>
2021-07-13 17:10:31 -0600

    Add UserRespository tests for function and instance

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,7 +2,49 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
+  let userRepository = new UserRepository({
+    id: 1,
+    name: 'Luisa Hane',
+    address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+    email: 'Diana.Hayes1@hotmail.com',
+    strideLength: 4.3,
+    dailyStepGoal: 10000,
+    friends: [
+      16,
+      4,
+      8
+      ]}, {
+    id: 2,
+    name: 'Jarvis Considine',
+    address: '30086 Kathryn Port, Ciceroland NE 07273',
+    email: 'Dimitri.Bechtelar11@gmail.com', 
+    strideLength: 4.5, 
+    dailyStepGoal: 5000,
+    friends: [
+      9,
+      18,
+      24,
+      19
+      ]}, {
+    id: 3,
+    name: 'Herminia Witting',
+    address": '85823 Bosco Fork, East Oscarstad MI 85126-5660',
+    email: 'Elwin.Tromp@yahoo.com',
+    strideLength: 4.4,
+    dailyStepGoal: 5000,
+    friends: [
+      19,
+      11,
+      42,
+      33
+      ]
+    })
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+
+  it('should be an instance of UserRepository', function() {
+    expect(userRepository).to.be.an.instanceof(UserRepository);
+  }); 
 });
