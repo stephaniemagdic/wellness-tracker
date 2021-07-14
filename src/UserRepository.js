7f5e4aecea9afb219bf4179465926b986e4db4b8
Sarah <sarahrudy@gmail.com>
2021-07-13 18:17:57 -0600

    Pseudocode for returnAverageStepGoal method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -3,17 +3,21 @@ class UserRepository {
     this.userData = userData;
   }
 
-  returnUserData(id) {
-
+  returnUserData(userId) {
+    return this.userData.find(userData => userData.id == userId)
   }
 
   returnAverageStepGoal() {
-
+    // The average step goal amongst all users
+    const average = this.userData.reduce((totalSteps, userData) => {
+      totalSteps += userData.dailyStepGoal
+      console.log("totalSteps: ", totalSteps)
+      console.log("userData: ", userData)
+      console.log("userData.dailyStepGoal: ", userData.dailyStepGoal)
+      return totalSteps 
+    }, 0)
+    return average / this.userData.length
+    }
   }
 
-
-  
-}
-
 export default UserRepository;
-
