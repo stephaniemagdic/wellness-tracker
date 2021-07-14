5c0a4819b500d6abb01fbf15954c366712b9f9cb
Sarah <sarahrudy@gmail.com>
2021-07-13 18:18:54 -0600

    Remove console.logs and commented out code

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -8,12 +8,8 @@ class UserRepository {
   }
 
   returnAverageStepGoal() {
-    // The average step goal amongst all users
     const average = this.userData.reduce((totalSteps, userData) => {
       totalSteps += userData.dailyStepGoal
-      console.log("totalSteps: ", totalSteps)
-      console.log("userData: ", userData)
-      console.log("userData.dailyStepGoal: ", userData.dailyStepGoal)
       return totalSteps 
     }, 0)
     return average / this.userData.length
