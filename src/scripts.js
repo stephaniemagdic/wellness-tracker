3f590635699bdae7f5076b8704c9ef00df040073
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-14 16:00:37 -0600

    Comment out logic in order to psuedocode and review api logic step by step as a team

--- src/scripts.js
+++ src/scripts.js
@@ -14,76 +14,84 @@ console.log('This is the JavaScript entry file - your code begins here.');
 import userData from './data/users';
 import UserRepository from './UserRepository';
 
-//fectch user data only to populate the new User Repository
+//new Promise(resolve, reject)
 
-//reusable function to fetch APIData
     function fetchData(type) {
-      const root = http://localhost:3001/api/v1/;
-      const url = `${root}type`;
-
+      const root = `http://localhost:3001/api/v1/`;
+      const url = `${root}${type}`;
       const promise = fetch(url)
-        .then(response => response.json());
-        console.log("response", response)
-        console.log("promise", promise)
+        .then(response => response.json()
+        )
+       
+        // .then(console.log("success!"))
+        // .catch(console.log("errror!"))
       return promise;
     }
+
+    const userTest = fetchData('users');
+    console.log(userTest);
+    
 
 //Step 2: grab the data and store for each type of data needed.
 //class thoughts:
 // fetch(url).then((response) =>response.json.then(data) => myFunc(data)
 
      //put this in a function and then return this so its not globally scoped.
-     const userRepoPromise = fetchData(users)
-     .then(data => console.log(data))
-     .catch(err => console.log("error message"));
+     
+  //    const userRepoPromise = fetchData(users)
+  //    .then(data => console.log(data))
+  //    .catch(err => console.log("error message"));
 
 
-   const hydrationRepoPromise = fetchData(hydration)
-     .then(data => console.log(data))
-     .catch(err => console.log(console.log(errorMessage));
+  //  const hydrationRepoPromise = fetchData(hydration)
+  //    .then(data => console.log(data))
+  //    .catch(err => console.log(console.log(errorMessage));
 
 
-   const sleepRepoPromise = fetchData(hydration)
-     .then(data => console.log(data))
-     .catch(err => /* do something else */);
+  //  const sleepRepoPromise = fetchData(hydration)
+  //    .then(data => console.log(data))
+  //    .catch(err => /* do something else */);
 
  
-const APIDataArray = Promise.all([userRepoPromise, hydrationRepoPromise, sleepRepoPromise]).then((values) => {
-console.log("done!");
+// const apiDataArray = Promise.all([userRepoPromise, hydrationRepoPromise, sleepRepoPromise]).then((values) => {
+// console.log("done!");
+
+
 //this will return an array
 //dont need .then()
-loadPage();
-});
+
+// loadPage();
+// });
 
 //---------------------EVENT LISTENER--------------------------------------//
    //event listener on load .. get API DATA <--this needs to be rethought
  
 //---------------------GLOBAL VARIABLES--------------------------------------//
-const welcomeName = document.getElementById("welcome-user");
-const address = document.getElementById("address");
-const strideLength = document.getElementById("stride-length-text");
+// const welcomeName = document.getElementById("welcome-user");
+// const address = document.getElementById("address");
+// const strideLength = document.getElementById("stride-length-text");
 
 // Step 3:
 
 //function to create card on page and an instance of user.
 
 
- const users = new UserRespository(userRepo);
+//  const users = new UserRespository(userRepo);
 
- const user1 = users.returnUserData(1)
- //destructure
- const currentUser = new User(user1);
+//  const user1 = users.returnUserData(1)
+//  //destructure
+//  const currentUser = new User(user1);
 
- function loadPage() {
-   loadUserCard(currentUser);
- }
-
-
- function loadUserCard(userObj) {
+//  function loadPage() {
+//    loadUserCard(currentUser);
+//  }
 
 
- welcomeName.innerHTML = `${userObj.returnFirstName()}`;
- address = `${userObj.address}`
+//  function loadUserCard(userObj) {
 
- }
+
+//  welcomeName.innerHTML = `${userObj.returnFirstName()}`;
+//  address = `${userObj.address}`
+
+//  }
 
