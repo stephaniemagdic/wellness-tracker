1aa39f097a2588fc04540b11d7f5963fc004ede1
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-15 14:58:44 -0600

    Add pseudocode for displaying hydration and sleep data

--- src/scripts.js
+++ src/scripts.js
@@ -28,55 +28,65 @@ const strideLength = document.querySelector(".stride-length-text");
 
 async function loadPage() {
   const dataSets = await fetchPageData();
-  await generateRepoClasses(dataSets);
+  generateRepoClasses(dataSets);
   loadPageInfo();
 }
 
 async function fetchPageData() {
-  const userRepoPromise = fetchData('users')
-  .then(console.log('success'))
+  const userRepoPromise = fetchData('users') 
+  // .then(console.log('success'))
   // .catch(err => console.log("error message"));
 
-  const hydrationRepoPromise = fetchData('hydration')
-    .then(console.log('success'));
+  const hydrationRepoPromise = fetchData('hydration')  
+    // .then(console.log('success'));
       //  .catch(err => console.log(console.log(errorMessage));
 
-  const sleepRepoPromise = fetchData('sleep')
-    .then(console.log('success'));
+  const sleepRepoPromise = fetchData('sleep')  
+    // .then(console.log('success'));
       //  .catch(err => /* do something else */);
 
-  const apiDataSets = await Promise.all([userRepoPromise, hydrationRepoPromise, sleepRepoPromise]).then((values) => {
-    return values;
-  });
+  const apiDataSets = await Promise.all([userRepoPromise, hydrationRepoPromise, sleepRepoPromise]).then(values => values);
 
   return apiDataSets;
 }
 
 function generateRepoClasses(dataSets) {
   //TO DO: Refactor-loop through argument to dry up.
+  console.log(dataSets);
   allUserData = new UserRepository(dataSets[0].userData);
   // allHydrationData = new HydrationRespository(dataSets[1].hydrationData);
   // allSleeperData = new SleepRepository(dataSets[2].sleepData);
 }
 
 async function fetchData(type) {
-  const root = `http://localhost:3001/api/v1/`;
-  const url = `${root}${type}`;
-  const promise = await fetch(url)
+  const promise = await fetch(`http://localhost:3001/api/v1/${type}`)
     .then(response => response.json())
     .then(data => data)
-    // .catch(console.log("errror!"))
+    // .catch(console.log("error!"))
   return promise;
 }
 
 function loadPageInfo() {
   //This will call invoke a function that loads our user card to start.
   displayUserCard();
+  // diplayHydrationInfo()
+    //create instance of hydration repo class
+    //create instance of hydration (holds one users hydration info)
+        //because we have to call methods on hydration class to get data in
+        //order to display this on the dom.
+  // displaysleepInfo()
+    //create instance of sleep repo class
+      //create instance of sleep class (holds one users sleep info)
+          //because we have to call methods on sleep class to get data in
+          //order to display this on the dom.
 }
 
 function displayUserCard() {
+  //create instance of the userRepo.
    const user1 = allUserData.returnUserData(1)
+   //create instance of user.
    const currentUser = new User(user1);
+   //use that info to display properties on the dom.
    welcomeName.innerHTML = `${currentUser.returnFirstName()}`;
    address.innerHTML = `${currentUser.address}`;
    strideLength.innerHTML = `${currentUser.strideLength}`
