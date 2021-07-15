b7968d89533ef8ce59b062803d98802aba823d85
Sarah <sarahrudy@gmail.com>
2021-07-15 16:49:57 -0600

    Add initial test structure to testing suite

--- test/HydrationRepo-test.js
+++ test/HydrationRepo-test.js
@@ -1,3 +1,20 @@
 import { expect } from 'chai';
 import HydrationRepository from '../src/HydrationRepository';
 
+describe('HydrationRepository', () => {
+  let hydrationRepository
+
+  beforeEach(() => {
+    hydrationRepository = new HydrationRepository(data)
+  })
+
+  it('should be function', () => {
+    expect(HydrationRepository).to.be.a('function')
+  })
+
+  it('should be an instance of HydrationRepository', () => {
+    expect(hydrationRepository).to.be.an.instanceOf(HydrationRepository)
+  })
+
+})
+
