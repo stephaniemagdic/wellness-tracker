bcf67e7024f64c9171af8206e06a5bb8231a53f3
Sarah <sarahrudy@gmail.com>
2021-07-16 17:26:44 -0600

    Write numDailyOuncesDrankInWeek method and make tests pass

--- src/UserHydrationData.js
+++ src/UserHydrationData.js
@@ -4,22 +4,23 @@ class UserHydrationData {
   }
   // will need to return 2 pieces of data by id 
 
+
   
   returnOuncesDrank(date = (this.hydroData[this.hydroData.length - 1].date)) {
     return this.hydroData.find(hydroItem => hydroItem.date === date).numOunces;
   }
 
-  filterByDate(date = (this.hydroData[this.hydroData.length - 1].date)) {
-    //filterUserById(id)
-    //return fluid ounces they consumed for a specific day (identified by a date) by the last Day. 
-  }
-
-  filterByWeek(id, week) {
+  numDailyOuncesDrankInWeek(specificDate = (this.hydroData[this.hydroData.length - 1].date)) {
   //how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
+    let dateIndex = this.hydroData.findIndex(day => day.date === specificDate)
+    let weekData = this.hydroData.slice(dateIndex - 6, dateIndex + 1)
+    return weekData.map(day => day.numOunces)
   }
-
 }
 
 export default UserHydrationData;
 
+// dashboard 
+// A display to show how much water they have consumed today (these displays are often called “widgets” in the FE tech world)
+// A display to show much water they have consumed each day over the course of the latest week
 
