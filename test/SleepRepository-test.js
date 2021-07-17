ecf1feda89b7a33c79590bcfa5b2fefd2478cb83
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-16 18:07:43 -0600

    Refactor syntax styling

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -8,9 +8,38 @@ describe('SleepRepository', function() {
 
   before(() => {
     data = [
-      {"userID":1,"date":"2019/06/15","hoursSlept":6.1,"sleepQuality":2.2},{"userID":2,"date":"2019/06/15","hoursSlept":7,"sleepQuality":4.7},{"userID":3,"date":"2019/06/15","hoursSlept":10.8,"sleepQuality":4.7},{"userID":1,"date":"2019/06/18","hoursSlept":10.4,"sleepQuality":3.1},{"userID":2,"date":"2019/06/18","hoursSlept":10.8,"sleepQuality":3.2},{"userID":3,"date":"2019/06/18","hoursSlept":9.8,"sleepQuality":2.6}
+      {
+        userID: 1,
+        date: '2019/06/15',
+        hoursSlept: 6.1,
+        sleepQuality: 2.2},
+      {
+        userID: 2,
+        date: '2019/06/15',
+        hoursSlept: 7,
+        sleepQuality: 4.7},
+      {
+        userID: 3,
+        date: '2019/06/15',
+        hoursSlept: 10.8,
+        sleepQuality: 4.7},
+      {
+        userID: 1,
+        date: '2019/06/18',
+        hoursSlept: 10.4,
+        sleepQuality: 3.1},
+      {
+        userID: 2,
+        date: '2019/06/18',
+        hoursSlept: 10.8,
+        sleepQuality: 3.2},
+      {
+        userID: 3,
+        date: '2019/06/18',
+        hoursSlept: 9.8,
+        sleepQuality: 2.6
+      }
     ];
-
     allSleepData = new SleepRepository(data);
   });
  
@@ -33,7 +62,20 @@ describe('SleepRepository', function() {
 
   it('should return an array of sleepItems by user', () => {
     const userSleepData = allSleepData.returnUserData(1);
-    expect(userSleepData).to.deep.equal([ {"userID":1,"date":"2019/06/15","hoursSlept":6.1,"sleepQuality":2.2}, {"userID":1,"date":"2019/06/18","hoursSlept":10.4,"sleepQuality":3.1}]);
+    expect(userSleepData).to.deep.equal([ 
+      {
+        userID: 1,
+        date: '2019/06/15',
+        hoursSlept: 6.1,
+        sleepQuality: 2.2
+      }, 
+      {
+        userID: 1,
+        date: '2019/06/18',
+        hoursSlept: 10.4,
+        sleepQuality: 3.1
+      }
+    ]);
   });
 });
 
--- test/UserSleepData-test.js
+++ test/UserSleepData-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import UserSleepData from '../src/Sleep';
+import UserSleepData from '../src/UserSleepData';
 
 describe('UserSleepData', function() {
   let user1SleepData;
@@ -7,16 +7,55 @@ describe('UserSleepData', function() {
 
   before(() => {
     data = [
-      {userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2}, 
-      {userID: 1, date: '2019/06/16', hoursSlept: 4.1, sleepQuality: 3.8},
-      {userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.6},
-      {userID: 1, date: '2019/06/18', hoursSlept: 10.4, sleepQuality: 3.1},
-      {userID: 1, date: '2019/06/19', hoursSlept: 10.7, sleepQuality: 1.2},
-      {userID: 1, date: '2019/06/20', hoursSlept: 9.3, sleepQuality: 1.2},
-      {userID: 1, date: '2019/06/21', hoursSlept: 7.8, sleepQuality: 4.2},
-      {userID: 1, date: '2019/06/22', hoursSlept: 7, sleepQuality: 3},
+      {
+        userID: 1, 
+        date: '2019/06/15', 
+        hoursSlept: 6.1, 
+        sleepQuality: 2.2
+      }, 
+      {
+        userID: 1, 
+        date: '2019/06/16', 
+        hoursSlept: 4.1, 
+        sleepQuality: 3.8
+      },
+      {
+        userID: 1, 
+        date: '2019/06/17', 
+        hoursSlept: 8, 
+        sleepQuality: 2.6
+      },
+      {
+        userID: 1, 
+        date: '2019/06/18', 
+        hoursSlept: 10.4, 
+        sleepQuality: 3.1
+      },
+      {
+        userID: 1, 
+        date: '2019/06/19', 
+        hoursSlept: 10.7, 
+        sleepQuality: 1.2
+      },
+      {
+        userID: 1, 
+        date: '2019/06/20', 
+        hoursSlept: 9.3, 
+        sleepQuality: 1.2
+      },
+      {
+        userID: 1, 
+        date: '2019/06/21', 
+        hoursSlept: 7.8, 
+        sleepQuality: 4.2
+      },
+      {
+        userID: 1, 
+        date: '2019/06/22', 
+        hoursSlept: 7, 
+        sleepQuality: 3
+      },
     ];
-  
     user1SleepData = new UserSleepData(data);
   });
  
