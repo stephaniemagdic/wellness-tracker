3b1aa04fd45dbd54939170ab59cf6d8aa7bbca74
Sarah <sarahrudy@gmail.com>
2021-07-17 21:58:48 -0600

    Rename filterHydroUserData method to returnUserData

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -4,51 +4,51 @@ import HydrationRepository from '../src/HydrationRepository';
 describe('HydrationRepository', () => {
   let hydrationRepository
   let allHydroData = [{
-    userID: 1,
-    date:"2019/06/15",
-    numOunces: 37
-  },
-  {
-    userID: 2,
-    date:"2019/06/15",
-    numOunces: 75
-  },
-  {
-    userID: 3,
-    date:"2019/06/15",
-    numOunces: 47
-  },
-  {
-    userID: 1,
-    date:"2019/06/16",
-    numOunces: 69
-  },
-  {
-    userID: 2,
-    date:"2019/06/16",
-    numOunces: 91
-  },
-  {
-    userID: 3,
-    date:"2019/06/16",
-    numOunces: 99
-  },
-  {
-    userID: 1,
-    date:"2019/06/17",
-    numOunces: 96
-  },
-  {
-    userID: 2,
-    date:"2019/06/17",
-    numOunces: 96
-  },
-  {
-    userID: 3,
-    date:"2019/06/17",
-    numOunces: 28
-  }
-];
+      userID: 1,
+      date:"2019/06/15",
+      numOunces: 37
+    },
+    {
+      userID: 2,
+      date:"2019/06/15",
+      numOunces: 75
+    },
+    {
+      userID: 3,
+      date:"2019/06/15",
+      numOunces: 47
+    },
+    {
+      userID: 1,
+      date:"2019/06/16",
+      numOunces: 69
+    },
+    {
+      userID: 2,
+      date:"2019/06/16",
+      numOunces: 91
+    },
+    {
+      userID: 3,
+      date:"2019/06/16",
+      numOunces: 99
+    },
+    {
+      userID: 1,
+      date:"2019/06/17",
+      numOunces: 96
+    },
+    {
+      userID: 2,
+      date:"2019/06/17",
+      numOunces: 96
+    },
+    {
+      userID: 3,
+      date:"2019/06/17",
+      numOunces: 28
+    }
+  ];
   
   beforeEach(() => {
     hydrationRepository = new HydrationRepository(allHydroData)
@@ -67,12 +67,12 @@ describe('HydrationRepository', () => {
   })
 
   it('should return all of one user\'s hydration data by id', () => {
-    const allHydroDataUserOne = hydrationRepository.filterUserHydroData(1)
+    const allHydroDataUserOne = hydrationRepository.returnUserData(1)
     expect(allHydroDataUserOne).to.deep.equal([allHydroData[0], allHydroData[3], allHydroData[6]])  
   })
 
   it('should return all of a different user\'s hydration data by id', () => {
-    const allHydroDataUserTwo = hydrationRepository.filterUserHydroData(2)
+    const allHydroDataUserTwo = hydrationRepository.returnUserData(2)
     expect(allHydroDataUserTwo).to.deep.equal([allHydroData[1], allHydroData[4], allHydroData[7]])
   })
 })
